52015ad9dae3f3335387f001ddcfe673f91d931d
tatra-labs <tatra.labs@gmail.com>
2025-07-02 17:16:47 -0400

    initial structure app.py

---
+++ app.py
@@ -0,0 +1,57 @@
+import chainlit as cl 
+import logging 
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s") 
+
+@cl.on_chat_start 
+async def on_chat_start():
+    """Initialize the chat session.""" 
+    cl.user_session.set(
+        "messages",
+        [
+            {
+                "role": "system",
+                "content": "You are a helpful assistant that answers questions based on the provided documents.",
+            }
+        ],
+    )
+    await cl.Message(
+        author="assistant", 
+        content="Hi, how can I help you today?"
+    ).send()
+
+@cl.on_message
+async def on_message(message: cl.Message):
+    """Continue the chat session, return AI Message."""
+    human_message = message.content 
+    messages = cl.user_session.get("messages", [
+            {
+                "role": "system",
+                "content": "You are a helpful assistant that answers questions based on the provided documents.",
+            }
+        ]) 
+    messages.append(
+        {
+            "role": "user",
+            "content": human_message,
+        }
+    )
+
+
+    ### TODO: Get the response from the pipeline 
+
+
+    ai_message = "TODO: Implement the response generation logic here."
+
+    messages.append(
+        {
+            "role": "assistant",
+            "content": ai_message,
+        }
+    )
+    cl.user_session.set("messages", messages)
+
+    response_msg = cl.Message(content=ai_message)
+    
+    await response_msg.send()
+
