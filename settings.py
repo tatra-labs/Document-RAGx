d25b2b3805133b30c8938a451539d731790a0cbb
tatra-labs <tatra.labs@gmail.com>
2025-07-04 06:46:26 -0400

    add vector_store, document_store paths to settings

--- settings.py
+++ settings.py
@@ -7,14 +7,15 @@ env = environ.Env(DEBUG=(bool, False))
 
 PROJECT_NAME = "Document-RAGx-v1.0" 
 
-EMBEDDING_MODEL = env("EMBEDDING_MODEL", default="all-mpnet-base-v2")
+EMBEDDING_MODEL = env("EMBEDDING_MODEL", default="models/all-mpnet-base-v2")
 LLM_MODEL = env("LLM_MODEL", default="http://localhost:11434/")
 
 INSTRUCTOR_BASE_URL = env("INSTRUCTOR_BASE_URL", default="http://localhost:11434/v1") 
 INSTRUCTOR_API_KEY = env("INSTRUCTOR_API_KEY", default="ollama")
 
 INDICES = env("INDICES", default="./indices")
-
+VECTOR_STORE_PATH = Path(str(INDICES)) / "vector_store" 
+DOCUMENT_STORE_PATH = Path(str(INDICES)) / "document_store"
 
 chunk_size=1000 
 chunk_overlap=200
